e2aa0a80dbcba97139e6b62aad494117392854f3
evgeny-ilushin <evgeny.v.ilushin@gmail.com>
2015-10-26 14:12:21 +0300

    stderr

--- bot/sysinfo1.sh
+++ bot/sysinfo1.sh
@@ -5,4 +5,12 @@
 uname_s=`uname -rv`
 uptime_s=`uptime | sed -e 's/ *[0-9\:]* //'`
 
-echo $uname_s \($uptime_s\)
+#echo $uname_s \($uptime_s\)
+
+#echo "Build info:"
+cd /home/olivka/olivka-github-project/olivka-darkbot
+C="$(git show -s 2>&1 | tail -n 1)"
+echo ""
+
+
+
